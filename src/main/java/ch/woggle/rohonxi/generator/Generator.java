7e8ef48207c6e67303a10f99002aff1d823cd115
moserth2 <moserth2@students.zhaw.ch>
2015-09-14 15:11:54 +0200

    Comment generators

--- src/main/java/ch/woggle/rohonxi/generator/Generator.java
+++ src/main/java/ch/woggle/rohonxi/generator/Generator.java
@@ -2,9 +2,42 @@ package ch.woggle.rohonxi.generator;
 
 import ch.woggle.rohonxi.alphabet.Alphabet;
 
+/**
+ * A Generator is used to generate a random matrix from a given alphabet.
+ * <p>
+ * The generated matrix' size can depend on the used generator, but can normally be set using the
+ * setSize method.
+ * 
+ * @author Thomas Moser
+ *
+ */
 public interface Generator {
+  /**
+   * Set the size of the generated output string.
+   * <p>
+   * If a generator implementation does not use the size property, a call to this method does not
+   * have any effect.
+   * 
+   * @param columns The number of columns in the output string
+   * @param rows The number of rows in the output string
+   */
+  public void setSize(int columns, int rows);
 
+  /**
+   * Sets the alphabet that is used for the string generation.
+   * <p>
+   * If a generator implementation does not allow to set an alphabet, a call to this method does not
+   * have any effect.
+   * 
+   * @param alphabet The used alphabet.
+   */
   public void setAlphabet(Alphabet alphabet);
 
+  /**
+   * Generate the random string with the previously set alphabet and the size
+   * 
+   * @return The generated random string, null if generation could not take place (e.g. no alphabet
+   *         or no size set).
+   */
   public String generate();
 }
--- src/main/java/ch/woggle/rohonxi/generator/SimpleRandomMatrixGenerator.java
+++ src/main/java/ch/woggle/rohonxi/generator/SimpleRandomMatrixGenerator.java
@@ -2,13 +2,43 @@ package ch.woggle.rohonxi.generator;
 
 import ch.woggle.rohonxi.alphabet.Alphabet;
 
+/**
+ * This Generator uses Math.random() to generate the output string.
+ * <p>
+ * Note that this generator does NOT use a cryptographically strong random generator.
+ * <p>
+ * Rows in the output string will be separated by a '\n' character.
+ * 
+ * @author Thomas Moser
+ *
+ */
 public class SimpleRandomMatrixGenerator implements Generator {
   private Alphabet alphabet;
-  private int width, height;
+  private int columns, rows;
 
-  public SimpleRandomMatrixGenerator(int width, int height) {
-    this.width = width;
-    this.height = height;
+  /**
+   * Create a generator without setting the output size. Output will be set to 10x10 and can be
+   * changed using the setSize method.
+   */
+  public SimpleRandomMatrixGenerator() {
+    this(10, 10);
+  }
+
+  /**
+   * Create a generator using a certain size for the output.
+   * 
+   * @param columns The number of characters per line of the output string
+   * @param rows The number of lines in the output string
+   */
+  public SimpleRandomMatrixGenerator(int columns, int rows) {
+    this.columns = columns;
+    this.rows = rows;
+  }
+
+  @Override
+  public void setSize(int columns, int rows) {
+    this.columns = columns;
+    this.rows = rows;
   }
 
   @Override
@@ -26,8 +56,8 @@ public class SimpleRandomMatrixGenerator implements Generator {
 
     StringBuilder matrix = new StringBuilder();
 
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
+    for (int y = 0; y < rows; y++) {
+      for (int x = 0; x < columns; x++) {
         int index = (int) (Math.random() * alphabetSize);
         matrix.append(alphabet.get(index));
       }
